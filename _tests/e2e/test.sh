894a19e136196c283be2ad4e0d803d2bf21fa24b
José Carlos Chávez <jcchavezs@gmail.com>
2021-01-26 17:59:29 +0100

    tests(e2e): improves the test output.

--- _tests/e2e/test.sh
+++ _tests/e2e/test.sh
@@ -12,6 +12,15 @@ curl -X POST http://localhost:9411/api/v2/spans \
 
 while [ ! -f $EXPORTED_TRACE ]; do sleep 1; done
 
+# We only read the last line as it is the one which is of our interest. Everytime
+# the collector outputs a trace into the file, it adds a new line with JSON rather
+# than creating an array of objects.
 TRACE_ID=$(tail -n 1 $EXPORTED_TRACE | jq -r ".resourceSpans[0].instrumentationLibrarySpans[0].spans[0].traceId")
 
-test "$TRACE_ID" = "cb5a198128c2f36138d3d48c4b72cd0e"
+if [ "$TRACE_ID" == "cb5a198128c2f36138d3d48c4b72cd0e" ]; then
+  echo "Trace ID has the expected value"
+else
+  echo "Unexpected trace ID \"$TRACE_ID\""
+  exit 1
+fi
+
