fe5804f3bc0b7752f58e24f3c68cbc0d1c1744f8
José Carlos Chávez <jcchavezs@gmail.com>
2021-02-10 21:45:03 +0100

    chore: removes unused test.

--- processors/piifilterprocessor/filters/regexmatcher/regex_test.go
+++ processors/piifilterprocessor/filters/regexmatcher/regex_test.go
@@ -14,10 +14,3 @@ func TestFilterMatchedKey(t *testing.T) {
 	assert.True(t, isModified)
 	assert.Equal(t, "***", redacted)
 }
-
-//func TestFilterStringValueRegexs(t *testing.T) {
-//	m, _ := NewMatcher(nil, []Regex{{Regexp: regexp.MustCompile("key_or_value")}}, nil)
-//	isModified, redacted := m.FilterMatchedKey(redaction.RedactRedactor, "http.request.header.password", "abc123", "")
-//	assert.True(t, isModified)
-//	assert.Equal(t, "***", redacted)
-//}
