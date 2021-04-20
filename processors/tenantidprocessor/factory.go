bc304319b18b7f9420b1db71f6f38bca8cd4fe7f
Mohit Agarwal <mohit@traceable.ai>
2021-04-20 16:17:08 +0530

    update Config to embed *config.ProcessorSettings for tenantidprocessor

--- processors/tenantidprocessor/factory.go
+++ processors/tenantidprocessor/factory.go
@@ -27,7 +27,7 @@ func NewFactory() component.ProcessorFactory {
 
 func createDefaultConfig() config.Processor {
 	return &Config{
-		ProcessorSettings: config.ProcessorSettings{
+		ProcessorSettings: &config.ProcessorSettings{
 			TypeVal: typeStr,
 			NameVal: typeStr,
 		},
