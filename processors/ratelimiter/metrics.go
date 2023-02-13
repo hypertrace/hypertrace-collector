7f72fef3767476448e11e40c6c464f5e5bc61c49
pavan-traceable <73101820+pavan-traceable@users.noreply.github.com>
2023-02-13 17:16:06 +0530

    Add rate limit service call count metric (#106)

--- processors/ratelimiter/metrics.go
+++ processors/ratelimiter/metrics.go
@@ -7,8 +7,9 @@ import (
 )
 
 var (
-	tagTenantID      = tag.MustNewKey("tenant-id")
-	droppedSpanCount = stats.Int64("tenant_id_dropped_span_count", "Number of spans dropped per tenant due to rate limiting", stats.UnitDimensionless)
+	tagTenantID                = tag.MustNewKey("tenant-id")
+	droppedSpanCount           = stats.Int64("tenant_id_dropped_span_count", "Number of spans dropped per tenant due to rate limiting", stats.UnitDimensionless)
+	rateLimitServiceCallsCount = stats.Int64("tenant_id_rate_limit_service_calls_count", "Number of calls to rate limiter service from collector", stats.UnitDimensionless)
 )
 
 func MetricViews() []*view.View {
@@ -22,7 +23,15 @@ func MetricViews() []*view.View {
 		TagKeys:     tags,
 	}
 
+	viewRateLimitServiceCallsCount := &view.View{
+		Name:        rateLimitServiceCallsCount.Name(),
+		Description: rateLimitServiceCallsCount.Description(),
+		Measure:     rateLimitServiceCallsCount,
+		Aggregation: view.Sum(),
+		TagKeys:     tags,
+	}
+
 	return []*view.View{
-		viewDroppedSpanCount,
+		viewDroppedSpanCount, viewRateLimitServiceCallsCount,
 	}
 }
